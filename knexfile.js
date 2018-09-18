410b3cdf6be9edf482983de8ee12c49ae71a2b25
jankilighthouse <jankipatel6890@gmail.com>
2018-09-18 23:56:23 +0000

    fix the migration

---
+++ knexfile.js
@@ -0,0 +1,49 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      // filename: './dev.sqlite3'
+    host : 'localhost',
+    user : 'development',
+    password : 'development',
+    database : 'test_db'
+
+    }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'test_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
